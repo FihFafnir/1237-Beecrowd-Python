2f97c40d7daee783ac2893a7d20dfe34f96a6d28
FihFafnir <fihfdragon@gmail.com>
2023-05-08 00:25:45 -0300

    Fixed

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ while True:
         
         # Verifica cada substring possível para encontrar a substring com maior comprimento
         for i in range(len(string_1)):
-            for j in range(len(string_1) - i):
-                # Pular substring menores ou de mesmo tamanho que a já registrada 
-                if i + 1 <= max_length:
-                    break
+            # Pular substring menores ou de mesmo tamanho que a já registrada 
+            if i + 1 <= max_length:
+                continue
 
+            for j in range(len(string_1) - i):
+                # Procura a substring dentro da segunda string
                 substring = string_1[j:j+i+1]
 
-                # Procura a substring dentro da segunda string
                 if substring in string_2:
                     max_length = len(substring)
                     
